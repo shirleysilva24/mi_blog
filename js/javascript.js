6189b393ff0bc2ac7c2373765df031e89e7cb773
shirleysilva24 <shisilmendoza@gmail.com>
2025-07-18 18:23:51 -0500

    Primer commit

---
+++ js/javascript.js
@@ -0,0 +1,3 @@
+// Mostrar solamente la fecha del día de hoy
+document.getElementById("Fecha").textContent = new Date().toLocaleDateString();
+// Donde toLocaleDateString sirve para mostrar la fecha en un formato legible.
